29597b3f97e85282b6863a88c75d7303c59eb6ea
ludihan <65617704+ludihan@users.noreply.github.com>
2023-11-24 08:15:09 -0300

    Initial commit

---
+++ questao5.R
@@ -0,0 +1,8 @@
+#A
+culpadoPositivo = 0.20 * 0.95
+culpadoNegativo = 0.20 * 0.05
+inocentePositivo = 0.80 * 0.20
+inocenteNegativo = 0.80 * 0.80
+
+#B P(C | P) = P(C ^ P) / P(P)
+(0.2 * 0.95) / ((0.2 * 0.95) + (0.8 * 0.2))
